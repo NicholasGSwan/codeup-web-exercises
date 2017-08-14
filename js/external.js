11b5a87961ac6b49fd52595d679d64691dbbe4a2
Uglygrayduck <Nicholasgswan@gmail.com>
2017-08-14 16:45:36 -0500

    complete first js exercises

---
+++ js/external.js
@@ -0,0 +1,6 @@
+"use strict";
+
+console.log("Hello from external JavaScript");
+alert("Welcome to my Website!");
+var favoriteColor = prompt("What is your favorite color?");
+alert("Oh gosh, " +favoriteColor+ " is my favorite color, too!")
