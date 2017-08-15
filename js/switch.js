4569554a088b522fe47060311fe6a9271d7e9a4b
Uglygrayduck <Nicholasgswan@gmail.com>
2017-08-15 15:00:12 -0500

    complete switch exercises

--- js/switch.js
+++ js/switch.js
@@ -36,3 +36,93 @@ switch(color) {
     // TODO: create a default case that will catch indigo and violet
     // TODO: for the default case, log: I do not know anything by that color.
 
+var shopperNames = ['Cameron', 'Ryan', 'George'];
+var shopperName = shopperNames[Math.floor(Math.random()*shopperNames.length)];
+
+console.log("Shopper is " + shopperName);
+
+switch (shopperName){
+    case "Cameron":
+    var itemsSubTotal = 180;
+    break;
+   case "Ryan":
+    itemsSubTotal = 240;
+    break;
+    case "George":
+    itemsSubTotal = 320;
+    break;
+}
+
+console.log(shopperName + "'s subtotal is $" + itemsSubTotal);
+
+if (itemsSubTotal >= 200){
+    var itemsGrandTotal = itemsSubTotal-(itemsSubTotal*.1);
+    console.log(shopperName + "'s total after discount is $" + itemsGrandTotal);
+}else{
+    itemsGrandTotal = itemsSubTotal;
+    console.log(shopperName + "'s total is $" + itemsGrandTotal + ". They did not get a discount.");
+}
+
+var flipACoin = Math.floor(Math.random()* 2);
+
+console.log(flipACoin)
+
+
+var buyAHouse = (flipACoin) ? console.log("Buy a house, dummy.") : console.log("Buy a car, I guess.")
+
+var luckyNumber = Math.floor(Math.random()* 6);
+var subTotal = 60;
+
+switch (luckyNumber){
+    case 0:
+        var grandTotal = subTotal;
+        console.log("Your total is $" + grandTotal);
+        break;
+    case 1:
+        grandTotal = subTotal - (subTotal*.1);
+        console.log("Your total is $" + grandTotal);
+        break;
+    case 2:
+        grandTotal = subTotal - (subTotal*.25);
+        console.log("Your total is $" + grandTotal);
+        break;
+    case 3:
+        grandTotal = subTotal - (subTotal*.33);
+        console.log("Your total is $" + grandTotal);
+        break;
+    case 4:
+        grandTotal = subTotal - (subTotal*.5);
+        console.log("Your total is $" + grandTotal);
+        break;
+    case 5:
+        grandTotal = 0;
+        console.log("Your order is free!");
+        break;
+    default:
+        console.log("Oh, what? That's weird....");
+        break;
+}
+
+var pickANumber = confirm ("Would you like to choose a number?");
+
+if (pickANumber === true) {
+    var numberPicked = prompt("Pick a number, any number:");
+} else {
+    alert("Oh, alright, I guess I'll leave you alone");
+}
+// prompt always returns a string!  if you need to use the number returned in a prompt
+// you need to parse it as an integer.
+if (isNaN(numberPicked)){
+    alert("That's not a number, boy-o")
+}else {
+
+    var numberEven = (numberPicked % 2 === 0) ? alert(numberPicked + " is even!") : alert(numberPicked + " is odd!");
+
+    var numberPlusOneHundred = 100 + parseInt(numberPicked);
+    alert(numberPicked + " plus 100 equals " + numberPlusOneHundred);
+    var numberIsPositive = (parseInt(numberPicked) >= 0) ? alert("You picked a positive number!") : alert("you picked a negative number!");
+}
+
+
+
+
