c98e9af1df122ce16c77bd5fdd83feb9e0b82fae
Uglygrayduck <Nicholasgswan@gmail.com>
2017-08-15 09:38:42 -0500

    added a closing semicolon

--- js/external.js
+++ js/external.js
@@ -3,4 +3,4 @@
 console.log("Hello from external JavaScript");
 alert("Welcome to my Website!");
 var favoriteColor = prompt("What is your favorite color?");
-alert("Oh gosh, " +favoriteColor+ " is my favorite color, too!")
+alert("Oh gosh, " +favoriteColor+ " is my favorite color, too!");
