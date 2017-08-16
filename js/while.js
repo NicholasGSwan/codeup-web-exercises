4783203c211ef0a4c2772c0ee764811a344eaeeb
Uglygrayduck <Nicholasgswan@gmail.com>
2017-08-16 14:44:18 -0500

    finished while loop exercises

---
+++ js/while.js
@@ -0,0 +1,27 @@
+"use strict";
+
+var whileLessThan = 2;
+
+while (whileLessThan <= 65536){
+    console.log(whileLessThan);
+    whileLessThan *= 2;
+}
+
+// This is how you get a random number between 50 and 100
+var allCones = Math.floor(Math.random() * 50) + 50;
+console.log("I have to sell " + allCones + " cones! Let's do it!")
+
+while (allCones > 0){
+    // This is how you get a random number between 1 and 5
+    var cones = Math.floor(Math.random() * 5) + 1;
+    if (cones > allCones){
+        console.log("I can't sell you " + cones + " cones because I only have " + allCones + " cones left.");
+    }else if (cones <= allCones){
+        console.log(cones + " cones sold!");
+        allCones -= cones
+    }
+
+}
+console.log("I think we're out of cones!");
+
+
