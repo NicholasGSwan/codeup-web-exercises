ccb85bd23a562c0a31e55361fb1d64992f50a739
Uglygrayduck <Nicholasgswan@gmail.com>
2017-08-16 15:22:57 -0500

    continued with for loop exercises

---
+++ js/for_loops.js
@@ -0,0 +1,31 @@
+"use strict";
+
+var userNumber;
+
+
+
+while (isNaN(userNumber) || userNumber > 10 || userNumber < 1){
+
+    userNumber = prompt("Give a number between 1 and 10, please!");
+    if (isNaN(userNumber)){
+        alert("That's not a number!");
+    }else if (userNumber < 1 || userNumber > 10){
+        alert("That's not between 1 and 10!");
+    }else {
+        alert("Thanks! That will do!")
+        console.log("A good value has been given.")
+    }
+    userNumber = Number(userNumber);
+    console.log("The number chosen is " + userNumber);
+}
+
+for (var multiplier = 1; multiplier <= 10; multiplier++){
+    console.log(userNumber + "x" + multiplier + "=" + userNumber*multiplier)
+}
+
+
+for (var numberOfRandom = 0; numberOfRandom < 10; numberOfRandom++){
+    var randomNumbers = Math.floor(Math.random() * 180) + 20;
+    var oddOrEven = (randomNumbers % 2 === 0) ? "even" : "odd";
+    console.log(randomNumbers + " is " + oddOrEven);
+}
