b265f7e895b74d3106baaacfddae10ef8d51ca82
Uglygrayduck <Nicholasgswan@gmail.com>
2017-08-17 09:07:10 -0500

    finished break and continue exercise

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -3,11 +3,12 @@ var numberToSkip;
 
 while (true) {
     numberToSkip = prompt("Please enter an odd number between 1 and 50.");
-    if (numberToSkip % 2 === 0){
-        console.log("That's not odd, that's even!");
-
-    }else if (numberToSkip < 0 && numberToSkip > 50){
+    if (parseInt(numberToSkip) < 0) {
         console.log("That's not between 1 and 50!");
+    }else if(parseInt(numberToSkip) > 50){
+        console.log("That's not between 1 and 50!");
+    }else if (numberToSkip % 2 === 0){
+        console.log("That's not odd, that's even!");
     }else if (isNaN(numberToSkip)){
         console.log("That's not even a number!");
     }else {
