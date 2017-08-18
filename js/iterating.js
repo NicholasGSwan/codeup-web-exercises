66eac664f938e66daef33db1fad85205d74f4b8c
Uglygrayduck <Nicholasgswan@gmail.com>
2017-08-18 16:07:30 -0500

    did iterating exercises using for loop and foreach function

--- js/iterating.js
+++ js/iterating.js
@@ -14,7 +14,7 @@ console.log(names.length);
     for (var i = 0; i < names.length; i++ ){
         console.log(names[i])
     }
-    names.forEach(function (element, index, array){
+    names.forEach(function (element){
         console.log(element)
     })
 
