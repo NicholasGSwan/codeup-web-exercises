350e311f9f2091690f1188edef6d140dcbacc55a
Uglygrayduck <Nicholasgswan@gmail.com>
2017-08-23 11:11:01 -0500

    add circle.js

---
+++ js/circle.js
@@ -0,0 +1,47 @@
+(function() {
+    "use strict";
+
+    // create a circle object
+    var circle = {
+        radius: 3,
+
+        getArea: function () {
+            var circleArea = Math.PI*Math.pow(circle.radius, 2);
+            // TODO: complete this method
+            // hint: area = pi * radius^2
+
+            return circleArea ; // TODO: return the proper value
+        },
+
+        logInfo: function (doRounding) {
+            // TODO: complete this method.
+
+            // If doRounding is true, round the result to the nearest integer.
+            // Otherwise, output the complete value
+            if (doRounding === true){
+                var circleArea2 = Math.round(circle.getArea());
+            }else {
+                circleArea2 = circle.getArea();
+            }
+
+
+            console.log("Area of a circle with radius: " + this.radius + ", is: " + circleArea2);
+        }
+    };
+
+    // log info about the circle
+    console.log("Raw circle information");
+    circle.logInfo(false);
+    console.log("Circle information rounded to the nearest whole number");
+    circle.logInfo(true);
+
+    console.log("=======================================================");
+    // TODO: Change the radius of the circle to 5.
+    circle.radius = 5;
+
+    // log info about the circle
+    console.log("Raw circle information");
+    circle.logInfo(false);
+    console.log("Circle information rounded to the nearest whole number");
+    circle.logInfo(true);
+})();
