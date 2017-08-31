34f20d6dc04537f9d225aeddf1d1909ee1c38600
Uglygrayduck <Nicholasgswan@gmail.com>
2017-08-31 14:47:26 -0500

    complete mouse events jquery exercises

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -2,8 +2,32 @@
 (function(){
 // $("#superneatparagraph").css("background-color", "yellow")
 // $(".codeup").css("border", "1px solid red")
-    $("li").css("font-size", "20px");
-    $("h1, p, li").css("background-color", "yellow");
-    alert($("h1").html());
+//     $("li").css("font-size", "20px");
+//     $("h1, p, li").css("background-color", "yellow");
+//     alert($("h1").html());
+
+//    variables here
+var headerSelector = $("h1");
+var paragraphSelector = $("p");
+var listItemSelector = $("li");
+
+//events here
+headerSelector.click(changeBackgroundToRed);
+paragraphSelector.dblclick(makeTextBigger);
+listItemSelector.hover(makeTextRed, changeTextBack);
+
+// functions here
+function changeBackgroundToRed(){
+  $(this).css("background-color", "red");
+};
+function makeTextBigger(){
+    $(this).css("font-size", "18px")
+}
+function makeTextRed(){
+    $(this).css("color", "red")
+}
+function changeTextBack(){
+    $(this).css("color", "black")
+}
 
 })();
