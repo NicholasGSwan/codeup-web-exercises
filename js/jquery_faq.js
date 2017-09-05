335d3684124b1a6aeeab5e12f38e1f4dc171a431
Uglygrayduck <Nicholasgswan@gmail.com>
2017-09-05 10:06:05 -0500

    complete jquery traversing exercise

--- js/jquery_faq.js
+++ js/jquery_faq.js
@@ -11,6 +11,19 @@
                 $("dt").removeClass("highlighted");
             }
             $(this).toggleClass("highlighted");
+        });
+        $("#ulHighlight").click(function(){
+            $(".National-Parks").each(function(){
+                $(this).children().last().toggleClass("highlighted");
+            })
+        });
+        $("h3").click(function(){
+            $(this).parent().children("li").toggleClass("bolded")
+
+        });
+        $("li").click(function(){
+            $(this).parent().children("li").first().toggleClass("blueFont")
         })
+
     })
 })();
